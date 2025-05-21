8c304d093b1f5aec148dbcde4b0992272c567e85
eidoxy <adriansondang@gmail.com>
2025-05-21 16:19:56 +0700

    refactor: streamline app and server setup by removing redundant code and organizing imports

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Application } from 'express';
+import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { errorHandler } from './middlewares/errorHandler';
 import routes from './routes';
+import multer from 'multer';
 
-const app: Application = express();
+const app = express();
+const upload = multer({ dest: 'public/uploads/' });
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(process.cwd(), 'public')));
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
